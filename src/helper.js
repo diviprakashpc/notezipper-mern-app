369a0eb7a4d9e31ae6eae9bf4717e2ac7f5f11a4
BlackieStars <diviprakashpc@gmail.com>
2024-08-15 19:21:33 +0530

    Vercel url api corrected

--- src/helper.js
+++ src/helper.js
@@ -1,2 +1,2 @@
 const PORT = process.env.REACT_APP_PORT || 5000
-export const API_BASE_URL = process.env.REACT_APP_NODE_ENV === 'production' ? 'https://notezipper-mern-api.vercel.app' : `http://localhost:${PORT}`
+export const API_BASE_URL = process.env.REACT_APP_NODE_ENV === 'production' ? 'https://notezipper-mern-api-opk4.vercel.app' : `http://localhost:${PORT}`
